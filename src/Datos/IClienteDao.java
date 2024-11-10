5e7b5317baca272a3e7794e2c796e8324c3fcf10
Raul <raulvaleriorolon2024@gmail.com>
2024-11-10 17:50:45 -0300

    insertar usuario

---
+++ src/Datos/IClienteDao.java
@@ -0,0 +1,9 @@
+package Datos;
+
+public interface IClienteDao {
+	
+	boolean insert(String nombre_usuario, int dni, String cuil, String nombre, String apellido, int sexo, String nacionalidad, 
+			String nacimiento, String domicilio, String localidad, int id_provincia, String email, String telefono, boolean activo);
+	
+
+}
